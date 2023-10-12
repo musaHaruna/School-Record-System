a3dae5fa4938838465c5bacecc36042b173de272
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-10-12 08:39:33 -0700

    landing-page-styling

---
+++ src/components/images.js
@@ -0,0 +1,12 @@
+import logo from '../assets/images/logo-icon.png'
+import schoolimg from '../assets/images/school-img.png'
+
+const Logo = () => {
+  return <img src={logo} alt='logo' />
+}
+const SchoolBg= () => {
+  return <img src={schoolimg} alt='logo' />
+}
+
+
+export { Logo, SchoolBg }
